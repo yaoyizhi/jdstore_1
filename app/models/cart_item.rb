459372bcf4ee9ec0c6e33ae5bb781830475899f7
yaoyizhi <email@yaoadou888>
2017-06-15 15:21:04 +0800

    implement current_cart function

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,13 +1,4 @@
 class CartItem < ApplicationRecord
-    def change
-        create_table :cart_items do |t|
-            t.integer :cart_id
-            t.integer :product_id
-            t.integer :quantity, default: 1
-            t.timestamps
-        end
-    end
-
     belongs_to :cart
     belongs_to :product
 end
