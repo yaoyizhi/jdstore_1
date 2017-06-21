8e5606b361e5bb730468d50680a1e5ea1f6fd4d3
yaoyizhi <email@yaoadou888>
2017-06-21 09:44:09 +0800

    add token to order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,7 @@ class OrdersController < ApplicationController
         @order.total = current_cart.total_price
         if @order.save
             current_cart.cart_items.each do |cart_item|
-                product_list = Product_list.new
+                product_list = ProductList.new
                 product_list.order = @order
                 product_list.product_name = cart_item.product.title
                 product_list.product_price = cart_item.product.price
@@ -13,17 +13,17 @@ class OrdersController < ApplicationController
                 product_list.save
             end
 
-            redirect_to order_path(@order)
+            redirect_to order_path(@order.token)
         else
             render 'carts/checkout'
       end
     end
-def show
-  @order = Order.find(params[:id])
-  @product_lists = @order.product_lists
-end 
 
-end
+    def show
+        @order = Order.find_by_token(params[:id])
+        @product_lists = @order.product_lists
+    end
+
     private
 
     def order_params
